d6e8c07bd92196d6570f54cfec5f157eeb4c861c
李荧峰123456 <liyingfeng8080@163.com>
2019-05-13 11:57:58 +0800

    swagger-ui.html

--- 1-2-swagger/src/main/java/com/lyf/swagger/entity/User.java
+++ 1-2-swagger/src/main/java/com/lyf/swagger/entity/User.java
@@ -1,10 +1,14 @@
 package com.lyf.swagger.entity;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class User {
+    @ApiModelProperty(value = "唯一id")
     private Long id;
+    @ApiModelProperty(value = "姓名")
     private String name;
+    @ApiModelProperty(value = "年龄")
     private Integer age;
 }
