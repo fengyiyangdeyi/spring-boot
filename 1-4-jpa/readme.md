dbad723bac85b93fdda6144423e7d25053b3f670
李荧峰123456 <liyingfeng8080@163.com>
2019-05-14 19:13:19 +0800

    log

---
+++ 1-4-jpa/readme.md
@@ -0,0 +1,2 @@
+####日志拼装sql参数
+####https://blog.csdn.net/blueheart20/article/details/52382052
--- 1-4-jpa/src/main/java/com/lyf/jpa/JpaApplication.java
+++ 1-4-jpa/src/main/java/com/lyf/jpa/JpaApplication.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @Slf4j
@@ -17,43 +18,43 @@ public class JpaApplication {
     }
 
 
-    @Bean
-    public CommandLineRunner demo(CustomerRepository repository) {
-        return (args) -> {
-            // save a couple of customers
-            repository.save(new Customer("Jack", "Bauer"));
-            repository.save(new Customer("Chloe", "O'Brian"));
-            repository.save(new Customer("Kim", "Bauer"));
-            repository.save(new Customer("David", "Palmer"));
-            repository.save(new Customer("Michelle", "Dessler"));
-
-            // fetch all customers
-            log.info("Customers found with findAll():");
-            log.info("-------------------------------");
-            for (Customer customer : repository.findAll()) {
-                log.info(customer.toString());
-            }
-            log.info("");
-
-            // fetch an individual customer by ID
-            repository.findById(1L)
-                    .ifPresent(customer -> {
-                        log.info("Customer found with findById(1L):");
-                        log.info("--------------------------------");
-                        log.info(customer.toString());
-                        log.info("");
-                    });
-
-            // fetch customers by last name
-            log.info("Customer found with findByLastName('Bauer'):");
-            log.info("--------------------------------------------");
-            repository.findByLastName("Bauer").forEach(bauer -> {
-                log.info(bauer.toString());
-            });
-            // for (Customer bauer : repository.findByLastName("Bauer")) {
-            // 	log.info(bauer.toString());
-            // }
-            log.info("");
-        };
-    }
+//    @Bean
+//    public CommandLineRunner demo(CustomerRepository repository) {
+//        return (args) -> {
+//            // save a couple of customers
+//            repository.save(new Customer("Jack", "Bauer"));
+//            repository.save(new Customer("Chloe", "O'Brian"));
+//            repository.save(new Customer("Kim", "Bauer"));
+//            repository.save(new Customer("David", "Palmer"));
+//            repository.save(new Customer("Michelle", "Dessler"));
+//
+//            // fetch all customers
+//            log.info("Customers found with findAll():");
+//            log.info("-------------------------------");
+//            for (Customer customer : repository.findAll()) {
+//                log.info(customer.toString());
+//            }
+//            log.info("");
+//
+//            // fetch an individual customer by ID
+//            repository.findById(1L)
+//                    .ifPresent(customer -> {
+//                        log.info("Customer found with findById(1L):");
+//                        log.info("--------------------------------");
+//                        log.info(customer.toString());
+//                        log.info("");
+//                    });
+//
+//            // fetch customers by last name
+//            log.info("Customer found with findByLastName('Bauer'):");
+//            log.info("--------------------------------------------");
+//            repository.findByLastName("Bauer").forEach(bauer -> {
+//                log.info(bauer.toString());
+//            });
+//            // for (Customer bauer : repository.findByLastName("Bauer")) {
+//            // 	log.info(bauer.toString());
+//            // }
+//            log.info("");
+//        };
+//    }
 }
